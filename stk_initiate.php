2cb3ac7f433f667b4c687e1bac289be0c0c26496
unknown <ericmutwiri4481@gmail.com>
2024-04-23 20:45:26 +0300

    new commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -23,10 +23,10 @@ if(isset($_POST['submit'])){
     for developer/test accounts, this money will be reversed automatically by midnight.
   */
   
-   $PartyA = $_POST['254705133720']; // This is your phone number, 
+   $PartyA = $_POST['phone']; // This is your phone number, 
   $AccountReference = '2255';
   $TransactionDesc = 'AVIATOR LTD';
-  $Amount = $_POST['1500'];
+  $Amount = $_POST['amount'];
  
   # Get the timestamp, format YYYYmmddhms -> 20181004151020
   $Timestamp = date('YmdHis');    
